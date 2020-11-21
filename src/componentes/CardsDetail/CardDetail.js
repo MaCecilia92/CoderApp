97d4af9be7a1c2d4e6a680e3e84d3321af60e07c
MaCecilia92 <cecialegre1992@gmail.com>
2020-11-21 16:56:53 -0300

    correción 21/11 desafío 6

--- src/componentes/CardsDetail/CardDetail.js
+++ src/componentes/CardsDetail/CardDetail.js
@@ -4,39 +4,35 @@ import CounterContainer from '../../containers/CounterContainer';
 import BuyButton from '../BuyButton/BuyButton'
 
 
-export default function CardDetail({books}) {
-    
+export default function CardDetail({libros}) {
   
     return (
         <div className="container">
      
-      {books.length === 0 ? (
+      {libros.length === 0 ? (
         <div style={{margin:'auto'}} >
         <Loader/>
         </div>
       ) : (
-        books.map( book =>
-        
-            <div key={book.id}>
-                <div className="row m-5">
-                    <div className="col-md-4">
+        libros.map( libro => <div key={libro.id}>
+            <div className="row m-4">
+                <div className="col-md-4">
 
-                    <img src={book.image} className="img-fluid" alt={book.title} style={{backgroundColor:'grey', height:'auto', width:'500px', backgroundImage:`url(${book.image})`, backgroundPosition:'center'}}/>
-                    <br></br>
-                    </div>
+                <img src={libro.image} className="img-fluid" alt={libro.title} style={{backgroundColor:'grey', height:'auto', width:'500px', backgroundImage:`url(${libro.image})`, backgroundPosition:'center'}}/>
+                <br></br>
+                </div>
 
-                    <div className="col-md-7">
-                        <h3>{book.title}</h3>
-                        <hr/>
-                        <p className="card-descripcion text-left">{book.description}</p>
-                        <div className="d-flex justify-content-center">
-                        <CounterContainer min='0' max='10'/>
-                        <BuyButton/>
-                        </div>
+                <div className="col-md-8">
+                    <h3>{libro.title}</h3>
+                    <hr/>
+                    <p className="card-descripcion text-left">{libro.description}</p>
+                    <div className="d-flex justify-content-center">
+                    <CounterContainer min='0' max='10'/>
+                    <BuyButton/>
                     </div>
                 </div>
             </div>
-            )
+        </div>)
       )}
       
     </div>
--- src/containers/CardDetailContainer.js
+++ src/containers/CardDetailContainer.js
@@ -3,29 +3,52 @@ import CardDetail from '../componentes/CardsDetail/CardDetail';
 
 export default function CardDetailContainer() {
 
-    const [books, setBooks]= useState([])
+    const [libros, setLibros]= useState([])
 
-    const getBooks = new Promise ((res,rej)=>{
-        const books = fetch('https://private-5709b2-booklists.apiary-mock.com/booklists');
+    useEffect(()=>{
+        const getBooks = fetch('https://private-5709b2-booklists.apiary-mock.com/booklists');
 
         setTimeout(()=>{
-            res(books)
-            console.log('Recibiendo respuesta')
-            rej("error")
+            getBooks
+            .then(res => {
+                    const results = res.json();
+                    return results;
+                })
+            .then(results => {
+                    console.log(results);
+                    setLibros([results[Math.floor((Math.random() * results.length) +1)]]);
+      });
         }, 4000)
-    })
 
-    useEffect(() => {
-        getBooks
-        .then((response) => response.json())
-        .then((json) => setBooks(json))
-        .catch((error) => console.error(error))
-        console.log(books);
-    }, [])
+    },[]);
 
     return (
         <div>
-            <CardDetail books={books}/>
+            <CardDetail libros={libros}/>
         </div>
     )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
